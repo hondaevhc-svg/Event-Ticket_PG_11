91632635821cd107807f0078d527c24bc9b7fe1c
hondaevhc-svg <hondaevhc@gmail.com>
2026-01-07 13:24:48 +0300

    Update streamlit_pg_11.py

--- streamlit_pg_11.py
+++ streamlit_pg_11.py
@@ -139,6 +139,7 @@ with st.sidebar:
             tickets.loc[:, ["Timestamp"]] = None
             save_tickets_df(tickets)
             st.success("✅ Database has been reset.")
+            st.session_state["admin_pass"] = ""
         else:
             st.error("❌ Incorrect Admin Password")
 
@@ -534,7 +535,9 @@ with tabs[3]:
                 menu = edited_menu_clean
 
                 st.success("✅ Menu and Inventory synchronized.")
+                st.session_state["menu_pass"] = ""
         else:
 
             st.error("❌ Incorrect Menu Password")
+
 
